cc5b370073a5f50cfe2b5d97df2d7a4bae6ff7b2
Alexander Tupavov <tauinbox@gmail.com>
2019-08-19 07:48:07 +0300

    regexp fix

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const filter = process.argv[3] ? process.argv[3].toUpperCase() : null;
 const clientRegExp = /^{"type":\s+"client".*}$/;
 const serverRegExp = /^<(\d{1,3})>(\d{0,2}) (\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z|-) ([\x00-\x7F]+|-) ([\x00-\x7F]+|-) ([\x00-\x7F]+|-) ([\x00-\x7F]+|-) (\[.*]|-)\s*(.*)$/;
 const structuredDataRegExp = /\[(.*?)]/g;
-const sdIdRegExp = /(.+)@[^\s]+/;
+const sdIdRegExp = /^(\w+)@?\w*/;
 const sdParamsRegExp = /(\w+)="((\w|\s)*)"/g;
 let lastTimestamp = '';
 let stacktraceMessage = '';
